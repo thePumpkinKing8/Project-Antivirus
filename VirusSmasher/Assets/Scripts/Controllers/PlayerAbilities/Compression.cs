e38c91e55780ad4a1e3b78fe78eab1532b1ce16b
Blake Laprade <lapradeblake@gmail.com>
2024-03-22 16:04:59 -0600

    Compression basic implimentation
    bugs: player unshrinks when moving left or right

--- VirusSmasher/Assets/Scripts/Controllers/PlayerAbilities/Compression.cs
+++ VirusSmasher/Assets/Scripts/Controllers/PlayerAbilities/Compression.cs
@@ -6,16 +6,34 @@ public class Compression : CollectablePower
 {
     private Vector3 _playerScale;
 
-    private bool _compressed;
+    private bool _compressed = false;
     protected override void Awake()
     {
         base.Awake();
-
+        _playerScale = _player.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(InputManager.Shrink.triggered)
+        {
+            Shrink();
+        }
+    }
+
+    private void Shrink()
+    {
+        //divides the players local scale by the "shrinkValue"
+        if (!_compressed)
+        {
+            _player.transform.localScale = _player.transform.localScale / _player.settings.shrinkValue;
+            _compressed = true;
+        }
+        else
+        {
+            _player.transform.localScale = _playerScale;
+            _compressed = false;    
+        }
     }
 }
