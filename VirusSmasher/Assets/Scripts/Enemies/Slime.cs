721a192a3c3e19751839ca7efec67476ccf00135
Blake Laprade <lapradeblake@gmail.com>
2024-04-02 14:20:11 -0600

    slime movement

--- VirusSmasher/Assets/Scripts/Enemies/Slime.cs
+++ VirusSmasher/Assets/Scripts/Enemies/Slime.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Slime : Enemy
 {
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +24,16 @@ public class Slime : Enemy
         
     }
 
+    public override void Patrol()
+    {
+        base.Patrol();
+
+        _rb.velocity = new Vector2(-_speed, _rb.velocity.y);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        _speed *= -1;
     }
 }
 
