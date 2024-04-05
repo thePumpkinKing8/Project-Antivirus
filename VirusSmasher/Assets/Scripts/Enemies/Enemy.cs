34884ec932dd198e90b1fb01138302dece4df86c
Blake Laprade <lapradeblake@gmail.com>
2024-04-05 17:49:48 -0600

    slime finished

--- VirusSmasher/Assets/Scripts/Enemies/Enemy.cs
+++ VirusSmasher/Assets/Scripts/Enemies/Enemy.cs
@@ -6,6 +6,26 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] protected float _damage = 20f;
 
+    protected EnemyState _state;
+    [SerializeField] protected float _waitTime;
+    protected float _timer;
+
+
+    protected virtual void Update()
+    {
+        switch (_state)
+        {
+            case EnemyState.Patrol:
+                Patrol();
+                break;
+            case EnemyState.Wait:
+                Wait(_waitTime);
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+        }
+    }
 
     //spawns and despawns object on death 
     //this prevents us from having to instantiate new enemies everytime a room loads
@@ -21,6 +41,17 @@ public class Enemy : MonoBehaviour
        gameObject.SetActive(true);
     }
 
+    /// <summary>
+    /// applies damage and force to player
+    /// </summary>
+    /// <param name="collision"></param>
+    protected virtual void HitPlayer(Collision2D collision)
+    {
+        Vector2 direction = -collision.relativeVelocity.normalized;
+        collision.gameObject.GetComponent<PlayerController>().OnHit(_damage, direction);
+        Debug.Log(direction);
+    }
+
     public virtual void Patrol()
     {
 
@@ -28,7 +59,9 @@ public class Enemy : MonoBehaviour
 
     public virtual void Wait(float time)
     {
-
+        _timer += Time.deltaTime;
+        if (_timer >= _waitTime)
+            _state = EnemyState.Patrol;
     }
 
     public virtual void Attack()
