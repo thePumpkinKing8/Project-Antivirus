f49ffaf3ded399a7d41de83d51dca8be205c2d0d
Ben <b.benton287@mybvc.ca>
2024-04-04 23:43:19 -0600

    Icon Scripts and proto enemy script added

---
+++ VirusSmasher/Assets/Scripts/Enemies/ProtoVirus.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtoVirus : Enemy
+{
+
+    protected VirusCounter Viruscounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Viruscounter = GameObject.Find("UI Manager").GetComponent<VirusCounter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D c)
+    {
+
+
+        if (c.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Vector2 direction = -c.relativeVelocity.normalized;
+            c.gameObject.GetComponent<PlayerController>().OnHit(_damage, direction);
+            Debug.Log(direction);
+            DeSpawn();
+
+            Viruscounter.UpdateScore(1);
+        }
+
+        if (c.gameObject.layer == LayerMask.NameToLayer("PlayerProjectile"))
+        {
+            DeSpawn();
+
+            Viruscounter.UpdateScore(1);
+        }
+
+    }
+
+
+
+}
