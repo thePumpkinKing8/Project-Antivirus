aef9774050b9dfa2dc771386911688353547d57e
Blake Laprade <lapradeblake@gmail.com>
2024-04-13 16:10:35 -0600

    Door update
    Doors now animate when player is close
    doors have new sprite
    
    new prefab that adds 2 preconnected doors to the scene to make hooking rooms up faster
    
    Doors change their name to the parent room to make organization easier

--- VirusSmasher/Assets/Scripts/RoomScripts/Door.cs
+++ VirusSmasher/Assets/Scripts/RoomScripts/Door.cs
@@ -1,26 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 
 public class Door : MonoBehaviour
 {
-    [SerializeField] private Door _connectedDoor;
+    public Door _connectedDoor;
     [HideInInspector] public Room parentRoom;
     private bool _isActive = true;
     [SerializeField] private Transform _exitLocation;
     [SerializeField] private float _triggerDistance;
+    private Animator _anim;
+
+    private bool _open = false;
+
+    
+
+    public bool Open
+    {
+        get
+        {
+            return _open;
+        }
+        set
+        {
+            if (_open != value)
+            {
+                _open = value;
+                Animate(_open);
+            }
+        }
+    }
+
+    public GameObject pair; //other door to spawn when created
+
+    [HideInInspector] public bool spawner = false;
+
+
+
 
     private void Awake()
     {
-        parentRoom = GetComponentInParent<Room>();
+       
+            parentRoom = GetComponentInParent<Room>();
+            gameObject.name = $"Door{parentRoom.name}"; //to help keep track of what doors connect to what
+            _anim = GetComponent<Animator>();
+        
+        
     }
+
+    
+    
+
 
     private void Update()
     {
-        if(_triggerDistance <= (GameManager.Instance.player.transform.position - transform.position).magnitude) 
-        {
-            //play open animation
-        }
+        
+        
+            if (_triggerDistance <= (GameManager.Instance.player.transform.position - transform.position).magnitude)
+                Open = false;
+
+            else
+                Open = true;
+        
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -41,4 +86,21 @@ public class Door : MonoBehaviour
     {
         RoomManager.Instance.LoadRoom(_connectedDoor.parentRoom, _connectedDoor);
     }
+
+    private void Animate(bool value)
+    {
+        if(value)
+        {
+            _anim.SetBool("Open",true);
+        }
+        else
+        {
+            _anim.SetBool("Open", false);
+        }
+    }
+
 }
+
+
+
+
