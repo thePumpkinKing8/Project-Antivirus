130d73529b9d2b0dadc3b0e1d6dabe7a23a29af0
Blake Laprade <lapradeblake@gmail.com>
2024-04-16 07:10:09 -0600

    Set up the save system and virus states

--- VirusSmasher/Assets/Scripts/Managers/GameManager.cs
+++ VirusSmasher/Assets/Scripts/Managers/GameManager.cs
@@ -10,6 +10,11 @@ public class GameManager : Singleton<GameManager>
 
     public Color32 color = Color.green;
 
+    private List<Virus> viruses;
+
+    [SerializeField] private SaveData _saveData;
+
+
     public void SetArea()
     {
         if (SceneManager.GetActiveScene().name == "PrototypeLevel")
@@ -18,6 +23,35 @@ public class GameManager : Singleton<GameManager>
         }
         Debug.Log(color);
         player.GetComponent<SpriteRenderer>().color = color;
+    }
+
+    //adds virus to list of dead
+    public void AddDeadVirus(Virus virus)
+    {
+        viruses.Add(virus);
+    }
+
+    //checks if virus is alive
+    public bool CheckVirus(Virus virus)
+    {
+        foreach(Virus dead in viruses)
+        {
+            if(virus == dead)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Save()
+    {
+        _saveData.Save(viruses, player.dashPower.Collected, player.compressionPower.Collected, player.shieldPower.Collected);
+    }
+
+    public void Load()
+    {
+        //set required values to those in saveData
     }
 
 }
